f2d2c614e86106a8291b55f893f7e9e9986b86e5
stolbin <stolbin@samsonpost.ru>
2021-09-15 21:05:12 +0300

    2. CandidateSerlvet. Создание кандидата. [#2513]

---
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -0,0 +1,19 @@
+package ru.job4j.dream.servlet;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.store.Store;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CandidateServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        Store.instOf().save(new Candidate(0, req.getParameter("name")));
+        resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
+    }
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -12,7 +12,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Store {
 
     private static final Store INST = new Store();
-    private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static final AtomicInteger POST_ID = new AtomicInteger(4);
+    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
@@ -43,5 +44,10 @@ public class Store {
     public void save(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void save(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 }
