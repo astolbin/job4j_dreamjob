c3321a222f697d3453a4996aa1880ab6e99c3676
stolbin <stolbin@samsonpost.ru>
2021-11-10 21:42:22 +0300

    4. Ajax Jquery [#58534]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -10,7 +10,8 @@ import java.io.PrintWriter;
 public class GreetingServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
